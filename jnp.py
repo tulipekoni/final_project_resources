099d266ec1b30bf6b343c32e4ff15fd77b88a6ff
Joonas Konttila <joonas.konttila@siiili.com>
2025-12-18 23:29:00 +0200

    epochs to 20

--- jnp.py
+++ jnp.py
@@ -343,7 +343,7 @@ if __name__ == "__main__":
         
         train_one_backbone(
             backbone, train_csv, val_csv, test_csv, train_image_dir, val_image_dir, test_image_dir,
-            epochs=10, batch_size=32, lr=1e-4, img_size=256, pretrained_backbone=pretrained_backbone,
+            epochs=20, batch_size=32, lr=1e-4, img_size=256, pretrained_backbone=pretrained_backbone,
             freeze_backbone=freeze_backbone, loss=loss
         )
 
